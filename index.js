3341c690b10772aa0d734ebb6904ade9711bf1e6
abel mengich <kipyegoabel2308@gmail.com>
2024-03-26 13:53:32 -0400

    test passed

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = 31
+ totalBatteries = batteryBatches.reduce((accumulate,currentBatch) =>{
+    return accumulator + currentBatch;
+},31);
+
+console.log(totalBatteries);
+
 
 // Code your solution here
